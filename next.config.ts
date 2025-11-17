2ed4439b0a63966bfeee4ff163658cb9c87f66b0
software-boop <balaji@brihaspathi.com>
2025-11-17 14:19:13 +0530

    Enable Edge runtime for Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    runtime: "edge",
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
+  }
 };
 
 export default nextConfig;
